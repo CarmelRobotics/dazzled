2ceb514a4a132a2168630ea630a10147082c5623
toastertaster <toastertaster@gmail.com>
2013-12-17 19:37:31 -0800

    Adding initial code file to the project

---
+++ dazzled.ino
@@ -0,0 +1,25 @@
+/*
+ * Dazzled - by FIRST Team 2035
+ *
+ * Arduino program that:
+ * - Controls LEDs
+ * - Uses Networktables
+ *
+ */
+
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+ #define PSTR // Make Arduino Due happy
+#endif
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+void setup() {
+}
+
+void loop() {
+}
+
