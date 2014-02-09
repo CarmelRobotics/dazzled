b065bc010134eb36a9f8f61907bef18f3e3bf563
toastertaster <toastertaster@gmail.com>
2014-02-08 22:17:34 -0800

    Adding the arduino-side server for the cRIO to communicate to.

---
+++ simpleserver/simpleserver.ino
@@ -0,0 +1,68 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+int currentLEDpattern = 0;
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+IPAddress ip(10,20,35,12);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use
+EthernetServer server(1140);
+
+void setup() {
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c); // this acknowledges it to the cRIO
+        if (c == '0') {
+	    // do nothing
+        } else if (c == '1') {
+	    currentLEDpattern = 1;
+	    pattern1();
+	} else if (c == '2') {
+	    currentLEDpattern = 2;
+	    pattern2();
+	}
+
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+void pattern1() {
+    Serial.println("Pattern 1");
+}
+
+void pattern2() {
+    Serial.println("Pattern 2");
+}
